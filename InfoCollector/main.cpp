fa1805b92f2e59f9776df9617e6dea388133018e
Adil Puthussery Shibu <78339800+TAGGART007@users.noreply.github.com>
2026-03-08 15:50:23 -0400

    Add files via upload

---
+++ InfoCollector/main.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+/*{
+    string name;
+    int id;
+    string email;
+    string collagename;
+    int phno;
+
+    cout<<"Enter the name of the student \n";
+    cin>>name;
+    cout<<"Enter the id \n";
+    cin>>id;
+    cout<<"Enter the email \n";
+    cin>>email;
+    cout<<"Enter collage name\n";
+    cin>>collagename;
+    cout<<"Enter thee phone no\n";
+    cin>>phno;
+
+    cout<<"\n Name of the student :: "<<name;
+    cout<<"\n Id :: "<<id;
+    cout<<"\n Email Id :: "<<email;
+    cout<<"\n Collage Name :: "<<collagename;
+    cout<<"\n phone no :: "<<phno;
+    cout << "\n Hello world!" << endl;
+    return 0;
+}*/
+/*{
+    string cardname;
+    int cardno;
+    int validthu;
+    int cvv;
+
+
+    cout<<"Enter the name on card \n";
+    cin>>cardname;
+    cout<<"Enter the card number \n";
+    cin>>cardno;
+    cout<<"Valid till \n";
+    cin>>validthu;
+    cout<<"Enter the CVV\n";
+    cin>>cvv;
+
+    cout<<"\n Card holder name :: "<<cardname;
+    cout<<"\n Card number :: "<<cardno;
+    cout<<"\n Card valid till :: "<<validthu;
+    cout<<"\n CVV number :: "<<cvv;
+    cout << "\n Hello world!" << endl;
+    return 0;
+}*/
+/*{
+    string Bikename;
+    int cost;
+    int onroadcost;
+    string model;
+    int milage;
+    int CC;
+
+    cout<<"Enter the name of bike \n";
+    cin>>Bikename;
+    cout<<"Enter the model name \n";
+    cin>>model;
+    cout<<"CC of the bike \n";
+    cin>>CC;
+    cout<<"Cost of the bike \n";
+    cin>>cost;
+    cout<<"OnRoad Cost of the bike \n";
+    cin>>onroadcost;
+    cout<<"Milage of the bike \n";
+    cin>>milage;
+
+    cout<<"\n Bike name :: "<<Bikename;
+    cout<<"\n Model :: "<<model;
+    cout<<"\n CC :: "<<CC;
+    cout<<"\n Cost :: "<<cost;
+    cout<<"\n On Road Cost :: "<<onroadcost;
+    cout<<"\n Milage :: "<<milage;
+    cout << "\n Hello world!" << endl;
+    return 0;
+}*/
+{
+    string bookname;
+    int cost;
+    float pubdate;
+    string authorname;
+    string pubname;
+    int pages;
+
+    cout<<"Enter the name of Book \n";
+    cin>>bookname;
+    cout<<"Enter the Cost \n";
+    cin>>cost;
+    cout<<"Published date \n";
+    cin>>pubdate;
+    cout<<"Enter the name of the author \n";
+    cin>>authorname;
+    cout<<"Enter the name of publisher \n";
+    cin>>pubname;
+    cout<<"no of pages \n";
+    cin>>pages;
+
+    cout<<"\n Book name :: "<<bookname;
+    cout<<"\n cost :: "<<cost;
+    cout<<"\n Published date :: "<<pubdate;
+    cout<<"\n author name :: "<<authorname;
+    cout<<"\n publisher name :: "<<pubname;
+    cout<<"\n No of pages :: "<<pages;
+    cout << "\n Hello world!" << endl;
+    return 0;
+}
